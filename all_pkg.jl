24cfe4fc26f6e001a031fac6ed4f865fa6ff8a91
French Bear <FrenchBear38@outlook.com>
2024-05-03 14:07:34 +0200

    Updated script to import all packages

---
+++ all_pkg.jl
@@ -0,0 +1,35 @@
+# All using
+# Quick way to load common packages
+#
+# 2024-05-03    PV
+
+import Pkg
+
+function u(m::String)
+    Pkg.add(m)
+end
+
+u("BenchmarkTools")
+u("CSV")
+u("DataFrames")
+u("DataStructures")
+u("Dates")
+u("Distributed")
+u("Distributions")
+u("Format")
+u("Getopt")
+u("InteractiveUtils")
+u("LinearAlgebra")
+u("MethodAnalysis")
+u("OffsetArrays")
+u("Permutations")
+u("Plots")
+u("Printf")
+u("Random")
+u("Sockets")
+u("SpecialFunctions")
+u("Statistics")
+u("StatsPlots")
+u("Test")
+u("TOML")
+u("Unicode")
